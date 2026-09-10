05cce669faf108d9453739d28eca53a63b02f8f5
buanas <moh20mail@protonmail.com>
2026-09-10 08:18:24 +0300

    initialized repo and setup the script

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+### SPEICAL THANKS TO
+# 1. u/RrOoSsSsOo and their post on getting the arm build of steam. https://www.reddit.com/r/AsahiLinux/comments/1tk6zoe/steam_native_arm_aarch64_on_asahi_linux_fedora/
+# 2. I will use an older static link using a specific old steam version of the arm64 in which it will automatically update, if you want to grab the latest build look into both de-wim's and ubuntu asahi steam install scripts
+
+set -euo pipefail
+
