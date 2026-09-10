32feec0f8d29616339dc405c127cea358a90b237
buanas <moh20mail@protonmail.com>
2026-09-10 11:42:11 +0300

    fixed path files to reperesnt the actual online repo directory name

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,7 @@ while true; do
 read -p "This script fully removes both the zip and installed/extracted steam files. Are you sure you want to continue? (y/n) " choice
 case "${choice,,}" in
     y|yes )
-    rm -rf ~/.local/share/Steam ~/.steam ~/steam_aarch64/GE-Proton*.tar.gz ~/steam_aarch64/bins_linuxarm64_linuxarm64.zip
+    rm -rf ~/.local/share/Steam ~/.steam ~/asahi-arm64-steam/GE-Proton*.tar.gz ~/asahi-arm64-steam/bins_linuxarm64_linuxarm64.zip
         break ;;
     n|no ) 
         exit 0 ;;
