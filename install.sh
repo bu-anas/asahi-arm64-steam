1b7dd41a22a1f1f0c8d8a52473a32fc037ab7d31
buanas <moh20mail@protonmail.com>
2026-09-10 11:36:32 +0300

    mentioned valve in install.sh for their official manifest file if you want the latest build

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ### SPEICAL THANKS TO
 # 1. u/RrOoSsSsOo and their post on getting the arm build of steam. https://www.reddit.com/r/AsahiLinux/comments/1tk6zoe/steam_native_arm_aarch64_on_asahi_linux_fedora/
-# 2. I will use an older static link using a specific old steam version of the arm64 in which it will automatically update, if you want to grab the latest build look into both de-wim's and ubuntu asahi steam install scripts
+# 2. I will use an older static link using a specific old steam version of the arm64 (mentioned in the post) in which it will automatically update at install, if you want to grab the latest build look into both de-wim's and ubuntu asahi steam install scripts and valve's officical manifest 
 # Check if the target installation folder does NOT exist
 
 set -eo pipefail
